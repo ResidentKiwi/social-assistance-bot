966b53664d727cca020d5a85da14dccff586e8c1
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-18 13:14:55 -0300

    Update vestibular-guide.js

--- js/vestibular-guide.js
+++ js/vestibular-guide.js
@@ -1,8 +1,16 @@
-document.getElementById('main-content').innerHTML = `
-  <h2><i class="fas fa-graduation-cap"></i> Vestibulares e Concursos</h2>
-  <ul class="list-group">
-    <li class="list-group-item bg-dark text-light">ENEM – <a href="https://enem.inep.gov.br/" target="_blank" class="text-info">Inscrição</a></li>
-    <li class="list-group-item bg-dark text-light">Fuvest – <a href="https://www.fuvest.br/" target="_blank" class="text-info">Informações</a></li>
-    <li class="list-group-item bg-dark text-light">Concurso INSS – <a href="https://www.in.gov.br/" target="_blank" class="text-info">Edital</a></li>
-  </ul>
-`;
+export default function () {
+  document.getElementById('main-content').innerHTML = `
+    <h2><i class="fas fa-graduation-cap"></i> Vestibulares e Concursos</h2>
+    <ul class="list-group">
+      <li class="list-group-item bg-dark text-light">
+        ENEM – <a href="https://enem.inep.gov.br/" target="_blank" class="text-info">Inscrição</a>
+      </li>
+      <li class="list-group-item bg-dark text-light">
+        Fuvest – <a href="https://www.fuvest.br/" target="_blank" class="text-info">Informações</a>
+      </li>
+      <li class="list-group-item bg-dark text-light">
+        Concurso INSS – <a href="https://www.in.gov.br/" target="_blank" class="text-info">Edital</a>
+      </li>
+    </ul>
+  `;
+}
